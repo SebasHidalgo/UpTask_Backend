6c2fd0ccc6f450e284a6736e49dfa701175a0349
SebasHidalgo <sebas.hidalgo2004@gmail.com>
2024-08-11 12:50:57 -0600

    cambios

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,18 @@
-import { CorsOptions } from 'cors'
+import { CorsOptions } from "cors";
+
+import colors from "colors";
 
 export const corsConfig: CorsOptions = {
-    origin: function(origin, callback) {
-        const whitelist = [process.env.FRONTEND_URL]
-
-        if(process.argv[2] === '--api') {
-            whitelist.push(undefined)
-        }
-
-        if(whitelist.includes(origin)) {
-            callback(null, true)
-        } else {
-            callback(new Error('Error de CORS'))
-        }
+  origin: function (origin, callback) {
+    const whitelist = [process.env.FRONTEND_URL];
+    console.log(colors.bgMagenta(origin));
+    if (process.argv[2] === "--api") {
+      whitelist.push(undefined);
     }
-}
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Error de CORS"));
+    }
+  },
+};
