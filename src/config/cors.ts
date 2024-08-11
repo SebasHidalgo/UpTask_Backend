5d86ec979b7aa6fbfdb3c1c9c8e6b28ce99dcf08
SebasHidalgo <sebas.hidalgo2004@gmail.com>
2024-08-11 13:02:37 -0600

    pruebas

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,7 +5,8 @@ import colors from "colors";
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
     const whitelist = [process.env.FRONTEND_URL];
-    console.log(colors.bgMagenta(origin));
+    console.log(whitelist);
+    console.log(origin);
     if (process.argv[2] === "--api") {
       whitelist.push(undefined);
     }
